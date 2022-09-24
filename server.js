c859acb55aab1967008cb3528180d0cb3a1380a5
Igor Radovic <igor@Igors-MacBook-Pro.local>
2022-09-24 03:37:31 +0200

    add middleware

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-
 const path = require('path');
+var morgan = require('morgan');
+
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+app.use(morgan('combined'));
 
 /**
  * Middleware applied to all routes because it is above route definitions
@@ -13,7 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 const whitelist = ['http://127.0.0.1:3000', 'http://127.0.0.1:5500'];
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    // dev mode has no origin
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Prevented by CORS'));
@@ -30,5 +36,13 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {});
+
+app.all('*', (req, res) => {
+  res
+    .status(404)
+    .json({ error: 'All HTTP method catch route - 404 Page not found' });
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server up on PORT: ${PORT}`));
